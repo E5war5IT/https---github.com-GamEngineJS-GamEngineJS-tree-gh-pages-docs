c4a2514cf1f39a5f09ef1a7ad087915f0aa0e9cd
eduard.17072006 <eduard.17072006@gmail.com>
2022-10-09 20:58:54 +0300

    :memo: - `Add or update documentation.`

---
+++ github.js
@@ -0,0 +1,19 @@
+/* Fetching the data from the API and storing it in the variables. */
+let profile = fetch('https://api.github.com/users/E5war5IT', {
+    mode: 'no-cors'
+});
+
+/* The above code is fetching the data from the API and storing it in the variables. */
+profile.then(response => {
+    if(response.ok){
+        return response.json();
+    } else {
+        info.innerHTML = 'Something went Wrong :)'
+    }
+}).then(data => {
+    let avatar = data.avatar_url;
+    let name = data.name;
+    let bio = data.bio;
+    let followers = data.followers;
+
+});
